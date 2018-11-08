cf73941e066784699cdad514699922a3c432a2a8
Kevin Sutton <kevmsutton@gmail.com>
2018-11-08 17:14:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(array)
-  new_array = []
+ new_array = []
   array.each do |i|
   new_array.push(i*i)
 end
 return new_array
 end
+
+
+#def square_array(array)
+  #array.collect{|x| x*x }
+#end
