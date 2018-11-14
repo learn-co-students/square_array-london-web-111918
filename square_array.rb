b5b8a1278fc29f0f159d6ba63fd18108eddc031f
Andy Purbrick <andypurbrick@gmail.com>
2018-11-14 16:21:26 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each { |number| new_array << number ** 2 } # Shorthand syntax for a do-block
+  new_array # Return keyword is not necessary in the last line of a method
 end
